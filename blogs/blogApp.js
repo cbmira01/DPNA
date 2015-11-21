56e9b6feaed95cdbfad43162bb3a3db64c081e7a
Calvin Miracle <cbmira01@gmail.com>
2015-11-20 23:54:38 -0500

    blogApp credits, more text for blog posts.

--- blogs/blogApp.js
+++ blogs/blogApp.js
@@ -6,19 +6,19 @@
   Thanks to K. Scott Allen for an excellent Angular tutorial.
     https://www.youtube.com/playlist?list=PLBTXLYhPD8MHGMW-ZEvdAtkxyAz-N8Toj
     
-  Thanks to ... for idea on Foundation panel reflow.
-    ...
+  Thanks to Thomas Kilian for how to handle "DOM ready for Foundation reflow".
+    http://stackoverflow.com/questions/12240639/how-can-i-run-a-directive-after-the-dom-has-finished-rendering/12243086#12243086
     
-  Thanks to ... for idea on color cycling.
-    ...
+  Thanks to rchawdry for an idea contributing to color cycling.
+    http://stackoverflow.com/questions/24873335/ng-repeat-passing-index-value-to-a-function/24874018#24874018
     
-  Thanks to ... for idea on proper triggering on DOM completion.
-    ...    
+  Thanks to "briguy37" for Javascript UUID function.
+    http://jsfiddle.net/briguy37/2mvfd/       
 */
 
   var blogApp = angular.module("blogApp", ["ngRoute", "ngStorage"])
   
-  // Directive to handle reflow redering of Foundation panels.
+  // Directive to handle reflow rendering of Foundation panels.
   .directive('myReflowPanels', function($timeout) {
       return {
         link: function(scope, element, attrs) {
@@ -86,35 +86,35 @@
           date: "Thu, 01 Jan 2010 00:00:00 GMT-0400",
           name: "Steve's Sports Bar",
           title: "Bellarmine Knights Basketball",
-          text: "Great game last night! Check out this signed basketball! My first post..",
+          text: "Great game last night! Check out this signed basketball! My first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis arcu et diam maximus volutpat eget egestas quam. Duis aliquam, justo sit amet posuere aliquet, leo nunc blandit est, et ultrices diam risus eget lorem. Vivamus ornare purus quis sem commodo facilisis. In bibendum at lacus tempus convallis. Proin convallis, lectus et pellentesque tincidunt, nunc massa tincidunt dolor, in posuere nisi tellus at turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut ac tortor in ex sollicitudin elementum. Aliquam posuere porta ipsum. Integer non tincidunt erat. Sed condimentum sollicitudin arcu a vehicula.",
           image: "http://auction.steinersports.com/ItemImages/000002/SMITBKS000002_zoom_IMAGE1_238091_mid.jpeg"
         },
         { UUID: "068bcf68-eede-4e7b-bbb0-1fa648c74c02",
           date: "Thu, 01 Jan 2011 00:00:00 GMT-0400",
           name: "Shirley's Runway",
           title: "Deer Park Goth, Emo",
-          text: "Bring out your inner Goth! My first post.",
+          text: "Bring out your inner Goth! My first post. Duis ac orci massa. Aenean blandit congue ipsum eu sodales. Donec mollis augue nisi, venenatis dapibus sapien efficitur vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas eu lobortis felis, eu varius est. Donec at sem id erat tristique efficitur nec eu ipsum. Cras non facilisis libero, maximus dictum tortor.",
           image: "https://lh5.googleusercontent.com/UL2Arkl8JPdfpCWo5yy6p3Ihfw9yT2abI8t5XGITbBmpqxYf_B_fUZINv4ii5kNok_wtNsznZk1wTxgWySSTT2369EvbFLPYdFLvMPGjCCOf0TfTtKH7iatgXeF9PIHM6Q"
         },
         { UUID: "4fced98b-e779-4b2c-b4da-b2a53b5db879",
           date: "Thu, 01 Jan 2012 00:00:00 GMT-0400",
           name: "Steve's Sports Bar",
           title: "Baseball at Deer Park Stadium..",
-          text: "Baseball under the lights at our new stadium. No image to share, sorry! My second sports post.",
+          text: "Baseball under the lights at our new stadium. No image to share, sorry! My second sports post. Donec faucibus orci eu pulvinar condimentum. Etiam faucibus, orci ut commodo suscipit, lorem est tincidunt nisl, et sodales mauris mauris vel nulla. Curabitur vestibulum faucibus urna, quis blandit tellus faucibus a. Donec sapien neque, posuere eleifend gravida porttitor, ultricies ac nibh. Mauris urna diam, auctor quis risus quis, ullamcorper congue nibh. Nullam luctus dui id felis ornare, sed pulvinar ipsum commodo. Nulla nisl orci, sollicitudin et vulputate consectetur, venenatis vel dolor. Praesent tristique rutrum tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla molestie ultricies eleifend.",
           image: ""
         },
         { UUID: "0a8b39dd-b699-4665-912f-22639897c84b",
           date: "Thu, 01 Jan 2013 00:00:00 GMT-0400",
           name: "Shirley's Runway",
           title: "Have a colorful summer!",
-          text: "A very colorful new summer dress! My second post.",
+          text: "A very colorful new summer dress! My second post. Duis tincidunt vulputate nisl, at accumsan nulla bibendum blandit. Duis a libero quis ligula mollis finibus. Quisque venenatis sollicitudin feugiat. Donec magna felis, tristique a malesuada nec, congue ac neque. Cras rutrum tincidunt dolor nec rhoncus. Nulla augue neque, facilisis ornare felis eget, fringilla feugiat dui. Nullam scelerisque vestibulum nunc. Cras accumsan vehicula mi, ac fermentum tellus dictum ut. Proin at scelerisque orci. Nunc quis iaculis lacus. Ut ultrices sodales dapibus. Vestibulum dui lorem, dignissim eu urna ut, euismod volutpat mi. Ut at purus nulla. Donec euismod vestibulum rutrum.",
           image: "http://rivista-cdn.palmspringslife.com/Palm-Springs-Life/May-2009/Fashion-Week-El-Paseo-2009-Wrap/Monday-March-23-Pacifica-Fashion-Runway-Bar/mondaylead.jpg?ver=1404319378"
         },
         { UUID: "b180bdf2-7a3f-4798-aa97-ee888b69c455",
           date: "Thu, 01 Jan 2014 00:00:00 GMT-0400",
           name: "Shirley's Runway",
           title: "New look for Deer Park",
-          text: "Check out this interesting hair accessory! Third post for me.",
+          text: "Check out this interesting hair accessory! Third post for me. Pellentesque urna libero, fringilla et arcu eget, dignissim interdum libero. Pellentesque urna libero, vulputate id tortor et, aliquam porttitor nibh. Sed at efficitur enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam vel malesuada velit, quis tempus purus. Suspendisse faucibus elit non iaculis tincidunt. Curabitur blandit sem vitae dignissim bibendum. Donec et rutrum justo. In hac habitasse platea dictumst. Pellentesque ut commodo lectus.",
           image: "http://images.lacarmina.com/130325_tokyo_steampunk_club_steam_garden_party_fashion_japan_13.jpg"
         }
       ];
--- js/my-javascript.js
+++ js/my-javascript.js
@@ -1,6 +1,6 @@
 
 
-//  Foundation reactive features for small media.
+  //  Foundation reactive features for small media.
   $(window).on("load resize",function(e) {
     var isSmallMedia = Foundation.utils.is_small_only();
     var dpnaElement = document.getElementById("dpnaText");
@@ -17,7 +17,7 @@
     }
   });  //end on-resize
     
-// Configuration for Slick Carousel. 
+  // Configuration for Slick Carousel. 
   $(document).ready(function(){
     $('.featureCarousel').slick({
       accessibility: true,
@@ -29,3 +29,15 @@
       cssEase: 'linear'
     });
   });    
+
+  // UUID generator, thanks to http://jsfiddle.net/briguy37/2mvfd/
+  function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  };
+ 
